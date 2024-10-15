45c6be32bb5f93bd2f6af89ab0446b4776e92fd6
stephanegc <stef3594@gmail.com>
2024-10-15 19:49:33 +0200

    use xgb as default

--- MLhousingPrices/model.py
+++ MLhousingPrices/model.py
@@ -1,12 +1,12 @@
 import joblib
 
-def load_model(filename='MLhousingPrices/resources/rfr_tuned.pkl'):
+def load_model(filename='MLhousingPrices/resources/xgb_tuned.pkl'):
     pp = joblib.load(filename)
     return pp
 
 class ModelTrainer:
-    def __init__(self):
-        pass
+    def __init__(self, modelType: str):
+        str.modelType = modelType
 
     def train(self, X_train, y_train):
         pass

db9e02f08cc01312d55ffdfaf0e5cb1fc9e12490
stephanegc <stef3594@gmail.com>
2024-10-15 23:15:39 +0200

    update app for classes and add train endpoint

--- MLhousingPrices/app.py
+++ MLhousingPrices/app.py
@@ -10,16 +10,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-@app.post("/upload")
-def upload_file(file: UploadFile = File(...)):
-    df = pd.read_csv(file.file)
-    file.file.close()
-    return {"filename": file.filename}
+    return {"message": "Welcome to the MLhousingPrices FastAPI app, head to /docs to predict data and train models !"}
 
 @app.post("/predict") 
 def prediction(file: UploadFile = File(...)):
+    """
+     This endpoint uses the uploaded file to predict house_median_value using the tuned XGBRegressor model.  
+     Upload the file via the button.  
+     The response will contain a clickable 'Download file' button, which will include your input data and the predictions appended to it.
+    """
     X_pred = pd.read_csv(file.file)
     file.file.close()
 
@@ -39,6 +38,24 @@ def prediction(file: UploadFile = File(...)):
     )
     response.headers["Content-Disposition"] = "attachment; filename=export.csv"
     return response
+
+@app.post("/train") 
+def train(file: UploadFile = File(...),  modelType = 'knn'):
+    """
+     This endpoint uses the uploaded file to train a model.  
+     Set the modelType to either 'knn' (KNN Regressor), 'xgb' (XGBRegressor) or 'rfr' (RandomForestRegressor).  
+     The model will be hypertuned, and as a response you will get a message containing the R2 score and the selected parameters.
+    """
+    data = pd.read_csv(file.file)
+    file.file.close()
+
+    pp = preprocessor.Preprocessor(data, cluster=False, subset=True) 
+    pp.split().preprocess_train()
+
+    mt = model.ModelTrainer(modelType=modelType)
+    mt.train(X_train_pp=pp.X_train_pp, y_train=pp.y_train)
+
+    return {"Best R2 score" : mt.model.best_score_, "Best params" : mt.model.best_params_}
 
 
 if __name__ == "__main__":
